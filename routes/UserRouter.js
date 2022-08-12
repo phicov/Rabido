7c6194ae1d400f844e12bf004f3d3b896f4df7c2
kennethachan <kennethandrechan@gmail.com>
2022-08-12 11:20:13 -0400

    user has CRUD

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,11 +1,12 @@
-const Router = require('express').Router()
-const controller = require('../controllers/UserController')
+const Router = require("express").Router()
+const controller = require("../controllers/UserController")
 
-Router.get('/get-users', controller.GetUsers)
-Router.post('/new-user', controller.CreateUser)
-
+Router.get("/get-users", controller.GetUsers)
+Router.get("/:user_id", controller.GetUser)
+Router.post("/new-user", controller.CreateUser)
+Router.put("/:user_id", controller.UpdateUser)
+Router.delete("/:user_id", controller.DeleteUser)
 // Router.post('/register',
 // contr.Register)
-
 
 module.exports = Router
