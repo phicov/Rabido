31b94daa2c3dfc70314bc5d8165c5ff69329718b
“Philippe <philippehcov@gmail.com>
2022-08-12 13:13:43 -0700

    Profile Pic

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -24,8 +24,17 @@ function SignIn(props) {
   return (
     <div className="signInBody">
       <nav>
-        <Link className="link" to="/signup">
-          Sign Up
+        <Link className="link" to="/feed">
+          Feed
+        </Link>
+        <Link className="link" to="/categories">
+          Categories
+        </Link>
+        <Link className="link" to="/profile">
+          Profile
+        </Link>
+        <Link className="link" to="/">
+          Sign Out
         </Link>
       </nav>
       <div className="signin-container">
