63ff79571fab503183fb43bafdbbf6b336d1848a
Austin Do <austinndo@gmail.com>
2022-08-13 22:06:51 -0700

    SignUp new users with digested password now working

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,12 +1,11 @@
-const Router = require("express").Router()
-const controller = require("../controllers/UserController")
+const Router = require('express').Router()
+const controller = require('../controllers/UserController')
 
-Router.get("/get-users", controller.GetUsers)
-Router.get("/:user_id", controller.GetUser)
-Router.post("/new-user", controller.CreateUser)
-Router.put("/:user_id", controller.UpdateUser)
-Router.delete("/:user_id", controller.DeleteUser)
-// Router.post('/register',
-// contr.Register)
+Router.get('/get-users', controller.GetUsers)
+Router.get('/:user_id', controller.GetUser)
+Router.post('/new-user', controller.CreateUser)
+Router.put('/:user_id', controller.UpdateUser)
+Router.delete('/:user_id', controller.DeleteUser)
+Router.post('/register-user', controller.RegisterUser)
 
 module.exports = Router
