f8f2279b2120f6454dddf14d6d575e1c78878b55
kennethachan <kennethandrechan@gmail.com>
2022-08-16 12:12:09 -0400

    can now add and see skill in profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from "react"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
-const URL = 'http://localhost:3001'
+const URL = "http://localhost:3001"
 
 function Profile({ user, skills }) {
   let navigate = useNavigate()
@@ -12,17 +12,18 @@ function Profile({ user, skills }) {
   const [toggleProfile, setToggleProfile] = useState(0)
 
   const [formValues, setFormValues] = useState({
-    name: '',
-    image: '',
+    name: "",
+    image: "",
     city: user && user.city,
-    about: '',
+    about: "",
     contact: user && user.email,
-    rate: '',
-    projects: '',
-    skillId: ''
+    rate: "",
+    projects: "",
+    skillId: "",
   })
 
   const [profileData, setProfileData] = useState([])
+  const [profileSkill, setProfileSkill] = useState([])
 
   const getUsersProfile = async () => {
     const res = await axios.get(`${URL}/api/profiles/${user.profileId}`)
@@ -54,7 +55,7 @@ function Profile({ user, skills }) {
       contact: user.email,
       rate: formValues.rate,
       projects: formValues.projects,
-      skillId: formValues.skillId
+      skillId: formValues.skillId,
     }
 
     const newProfile = await axios
@@ -65,23 +66,30 @@ function Profile({ user, skills }) {
       })
 
     setFormValues({
-      name: '',
-      image: '',
+      name: "",
+      image: "",
       city: user.city,
-      about: '',
+      about: "",
       contact: user.email,
-      rate: '',
-      projects: '',
-      skillId: ''
+      rate: "",
+      projects: "",
+      skillId: "",
     })
 
     navigate(0)
+  }
+
+  const getProfSkill = async () => {
+    const res = await axios.get(`${URL}/api/profiles/${user.profileId}`)
+    console.log(res.data.Skill)
+    setProfileSkill(res.data.Skill)
   }
 
   if (!user) {
     return <div>Loading</div>
   }
   if (user.profileId != null && toggleProfile == 0) {
+    getProfSkill()
     getUsersProfile()
     setToggleProfile(1)
   } else if (user.profileId != null && toggleProfile == 1) {
@@ -94,7 +102,7 @@ function Profile({ user, skills }) {
             <h1 className="realName">Name: {profileData.name}</h1>
             <h3 className="contact">Contact: {profileData.contact}</h3>
             <h3 className="rates">Rate: {profileData.rate}</h3>
-            {/* <h3 className="skills">Skill: {profileData.Skill.name}</h3> */}
+            <h3 className="skills">Skill: {profileSkill.name}</h3>
             <h3 className="location">Location: {profileData.city}</h3>
             <h3 className="about">About: {profileData.about}</h3>
             <div className="projectsContainer">
