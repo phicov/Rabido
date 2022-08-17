44acca2dc2605fca83b7bdd3cc121ddeebf83546
kennethachan <kennethandrechan@gmail.com>
2022-08-17 09:59:55 -0400

    cleaned up code 8-17-22 10AM

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
