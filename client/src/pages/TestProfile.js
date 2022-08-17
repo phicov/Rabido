9081d2de85e3824ee887f0d7c842945f4e6d658a
“Philippe <philippehcov@gmail.com>
2022-08-17 07:45:45 -0700

    Profile Thiings Form

--- client/src/pages/TestProfile.js
+++ client/src/pages/TestProfile.js
@@ -17,20 +17,18 @@ return (
             </ul>
         </nav>
 
-        <div className='menu'>
-            <ul>
-                <li><a href='#'>Settings</a></li>
-            </ul>
-        </div>
 
         <section className='about' id='about'>
             
+
             <img classname='profilePic' src='https://i.pinimg.com/736x/93/45/89/934589f3aa2f266b260de8bfeb3ae1ab.jpg'/>
             <div className='menu'>
                 <ul>
-                    <li><a href='#'>Settings</a></li>
+                <li><a href='#'>Profile</a></li>
+                <li><a href='#'>Settings</a></li>
                 </ul>
             </div>
+            <div className='aboutItems'>
             <h2>Name</h2>
             <h3>Skills</h3>
             <h3>Location</h3>
@@ -38,6 +36,7 @@ return (
             <h3>Rates</h3>
             
             <p>lorem15lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 lorem15 </p>
+            </div>
         </section>
 
         <section className='projects' id='projects'>
@@ -77,6 +76,18 @@ return (
                     <hr></hr>
                     <img src='https://miro.medium.com/max/1000/1*wLubnBK3-31zDS0ppyU6wQ.png'/>
                     <p>description 6</p>
+                </div>
+                <div className='projectItem'>
+                    <h3>project card 6</h3>
+                    <hr></hr>
+                    <img src='https://miro.medium.com/max/1000/1*wLubnBK3-31zDS0ppyU6wQ.png'/>
+                    <p>description 6</p>
+                </div>  <div className='projectItem addProject'>
+                    <a href='#'>
+                    <h3>Add Project</h3>
+                    <hr></hr>
+                    <img src='https://images.emojiterra.com/twitter/v14.0/512px/2795.png'/>
+                    </a>
                 </div>
             </div>
         </section>
